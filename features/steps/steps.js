344ea01850cf78f9a8fbc51062a7fc552bfa6aa3
Jean Souza <jsouza20603@gmail.com>
2022-09-30 14:40:33 -0300

    dojo teste 1

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,16 +1,18 @@
-         const{ Given, When, Then } = require("@cucumber/cucumber");
-         const { expect } = require ('@playwright/test')
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { expect } = require('@playwright/test')
 
-         Given('que esteja na página esidiomas', async function () {
-           await page.goto('https://www.esidiomas.com.br/');
-         });
+Given('que esteja na página esidiomas', async function () {
+  await page.locator('xpath=//*[@id="app"]/main/header/div[2]/a/img');
+});
 
-         When('acessar a aba cursos', async function () {
-            await page.locator('li:nth-child(3) > div > a').click();
-          });
- 
+When('acessar a aba cursos', async function () {
+  await page.locator('li:nth-child(3) > div > a').click();
+});
 
-          Then('validar a página e deve conter o texto Aulas individuais ou em grupos pequenos.', async function () {
-             expect(await page.locator('p > span').first()).toContainText('Aulas individuais ou em grupos pequenos')
-          });
- 
+
+Then('validar a página e deve conter o texto Aulas individuais ou em grupos pequenos.', async function () {
+  const text = page.locator('[class*=block-grid] div[id=zC7QFe] p span');
+  await expect(text).toHaveText('Aulas individuais ou em grupos pequenos. Inglês para todas as oportunidades, com foco em comunicação. Seguem o ritmo do aluno  e abordam assuntos variados.');
+  await page.waitForTimeout(1500);
+});
+
--- features/support/globalSetup.js
+++ features/support/globalSetup.js
@@ -1,21 +1,24 @@
-const { BeforeAll, Before, AfterAll, After } = require("@cucumber/cucumber");
+const { setDefaultTimeout, BeforeAll, Before, AfterAll, After } = require("@cucumber/cucumber");
 const { chromium } = require("@playwright/test");
 
-BeforeAll(async() =>{
+setDefaultTimeout(60 * 10000);
+
+BeforeAll(async () => {
     global.browser = await chromium.launch({
-        headless:false
+        headless: false
     });
 });
 
-AfterAll(async() => {
-    await  global.browser.close();
+AfterAll(async () => {
+    await global.browser.close();
 });
 
-Before(async() => {
+Before(async () => {
     global.context = await global.browser.newContext();
     global.page = await global.context.newPage();
+    await page.goto('https://www.esidiomas.com.br/');
 });
 
-After(async() => {
+After(async () => {
     await global.page.close();
 });
